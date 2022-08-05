a89045d2a31c5323c0bee23043c7b9cd2819c819
robertgrc <robert.rodriguez.rgrc@gmail.com>
2022-08-05 18:38:24 -0400

    309. Mostrar las imagenes cargadas

--- src/journal/views/NoteView.jsx
+++ src/journal/views/NoteView.jsx
@@ -113,7 +113,7 @@ export const NoteView = () => {
                 </Grid>
 
                 {/* Image gallery */}
-                <ImageGallery />
+                <ImageGallery images={ note.imageUrls }/>
 
         </Grid>
     )
